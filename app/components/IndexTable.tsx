18b303e8f15ded4adad21f493e8a7814fad46048
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2024-04-30 18:31:34 +0200

    Ready to pusblish

--- app/components/IndexTable.tsx
+++ app/components/IndexTable.tsx
@@ -3,16 +3,7 @@ import React, { ChangeEvent, Key, useState } from "react";
 import { Box, Button, Modal, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Typography } from "@mui/material"
 import { PokemonData } from "../page";
 import Image from "next/image";
-
-const styleModal = {
-  position: 'absolute',
-  top: '20px',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  mx: 5,
-};
+import PokemonModal from "./PokemonModal";
 
 export default function IndexTable({list}:{list: PokemonData[]}) {
   const [openPokemonModal, setOpenPokemonModal] = useState<PokemonData | null | undefined>(null);
@@ -92,34 +83,7 @@ export default function IndexTable({list}:{list: PokemonData[]}) {
           />
         </TableContainer>
       </Box>
-      <Modal
-        sx={styleModal}
-        open={!!openPokemonModal}
-        onClose={() => setOpenPokemonModal(null)}>
-          <Box>
-            <Box display="flex">
-              <Button variant="contained" sx={{ position: 'absolute', top: '10px', right: '10px' }} onClick={() => setOpenPokemonModal(null)}>X</Button>
-              { openPokemonModal &&
-                <Image height={300} width={300} src={openPokemonModal?.image ? openPokemonModal.image : '/public/interrogation.png'} alt={openPokemonModal?.name} />
-              }
-              <Box ml={3}>
-                <Typography variant="overline" display="block" gutterBottom>Name: {openPokemonModal?.name}</Typography>
-                <Typography variant="overline" display="block" gutterBottom>Type/s: {openPokemonModal?.type.join(' ')}</Typography>
-                <Typography variant="overline" display="block" gutterBottom>Other Names</Typography>
-                {openPokemonModal && Object.entries(openPokemonModal.otherNames).map(([key, value]) => (
-                  <Typography variant="overline" display="block" gutterBottom>{key}: {value}</Typography>
-                ))}
-              </Box>
-              <Box ml={3}>
-                {openPokemonModal && Object.entries(openPokemonModal.stats).map(([key, value]) => (
-                  <Typography variant="overline" display="block" gutterBottom>{key}: {value}</Typography>
-                ))}
-              </Box>
-            </Box>
-            <Typography variant="overline" display="block" gutterBottom>Description:</Typography>
-            <Typography variant="overline" display="block" gutterBottom>{openPokemonModal?.description}</Typography>
-          </Box>
-      </Modal>
+      {openPokemonModal && <PokemonModal pokemon={openPokemonModal} setClose={() => setOpenPokemonModal(null)} /> }
     </>
   );
 }
--- app/components/PokemonModal.tsx
+++ app/components/PokemonModal.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, Modal } from "@mui/material";
+import { Box, Button, Modal, Typography } from "@mui/material";
 import { PokemonData } from "../page";
+import Image from "next/image";
 
 const styleModal = {
     position: 'absolute',
     top: '20px',
-    bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
@@ -14,31 +14,31 @@ const styleModal = {
 export default function PokemonModal ({pokemon, setClose}: {pokemon: PokemonData, setClose: () => void}) {
     return(
         <Modal
-        sx={styleModal}
+        sx={{...styleModal, background: 'white'}}
         open={!!pokemon}
-        onClose={() => setOpenPokemonModal(null)}>
+        onClose={() => setClose()}>
           <Box>
             <Box display="flex">
-              <Button variant="contained" sx={{ position: 'absolute', top: '10px', right: '10px' }} onClick={() => setOpenPokemonModal(null)}>X</Button>
-              { openPokemonModal &&
-                <Image height={300} width={300} src={openPokemonModal?.image ? openPokemonModal.image : '/public/interrogation.png'} alt={openPokemonModal?.name} />
+              <Button variant="contained" sx={{ position: 'absolute', top: '10px', right: '10px' }} onClick={() => setClose()}>X</Button>
+              { pokemon &&
+                <Image height={300} width={300} src={pokemon?.image ? pokemon.image : '/public/interrogation.png'} alt={pokemon?.name} />
               }
               <Box ml={3}>
-                <Typography variant="overline" display="block" gutterBottom>Name: {openPokemonModal?.name}</Typography>
-                <Typography variant="overline" display="block" gutterBottom>Type/s: {openPokemonModal?.type.join(' ')}</Typography>
+                <Typography variant="overline" display="block" gutterBottom>Name: {pokemon?.name}</Typography>
+                <Typography variant="overline" display="block" gutterBottom>Type/s: {pokemon?.type.join(' ')}</Typography>
                 <Typography variant="overline" display="block" gutterBottom>Other Names</Typography>
-                {openPokemonModal && Object.entries(openPokemonModal.otherNames).map(([key, value]) => (
+                {pokemon && Object.entries(pokemon.otherNames).map(([key, value]) => (
                   <Typography variant="overline" display="block" gutterBottom>{key}: {value}</Typography>
                 ))}
               </Box>
               <Box ml={3}>
-                {openPokemonModal && Object.entries(openPokemonModal.stats).map(([key, value]) => (
+                {pokemon && Object.entries(pokemon.stats).map(([key, value]) => (
                   <Typography variant="overline" display="block" gutterBottom>{key}: {value}</Typography>
                 ))}
               </Box>
             </Box>
             <Typography variant="overline" display="block" gutterBottom>Description:</Typography>
-            <Typography variant="overline" display="block" gutterBottom>{openPokemonModal?.description}</Typography>
+            <Typography variant="overline" display="block" gutterBottom>{pokemon?.description}</Typography>
           </Box>
       </Modal>
     )
